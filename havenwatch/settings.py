ff2674b5c6886caf7955cd8cfd0d0ee987383651
gustavth <gustavthor04@gmail.com>
2025-05-09 14:00:56 +0000

    updating the database in settings.py

--- havenwatch/settings.py
+++ havenwatch/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'havenwatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verklegt_namskeid_db',
+        'USER': 'verklegt_db_user',
+        'PASSWORD': 'k4yJ05JA3t5U7D3OkzXM4Flw',
+        'HOST': 'db-verklegt-namskeid-ii-eu-uo6ty7.postgres.database.azure.com',
+        'PORT': '5432'
     }
 }
 
