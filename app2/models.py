32f29890f525d5d6fee140f33abba776ec9d7b55
Haukuraj <haukarinn@gmail.com>
2025-05-12 12:02:01 +0000

    FUCK COMMUNISM

--- app2/models.py
+++ app2/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio=models.TextField(max_length=500, blank=True,null=True)
+    profile_picture= models.ImageField(upload_to='profile_pics/',default='default.jpg')
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
 
