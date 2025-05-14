ac3d0b0b4f0656f9250f6228697549944666c3b0
00fire <erikbus15@gmail.com>
2025-05-14 19:22:39 +0000

    Revert "Merge branch 'Haukur's-monkey-branch' of https://github.com/00fire/Mortgage-Mayhem into Haukur's-monkey-branch"
    This reverts commit d289b9e9a4333754f3b360c70678b5dff4cd33a8, reversing
    changes made to 8caf3f4aa45c553d3c43ce3a41c36991bbbc1f8c.

--- app2/forms.py
+++ app2/forms.py
@@ -2,7 +2,7 @@ from django import forms
 #from .models import User
 from .models import UserProfile
 from .models import Properties
-
+from .models import PurchaseOffer
 
 # app2/forms.py
 from django import forms
@@ -10,13 +10,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 from .models import UserProfile, Properties     # import any other models you need
+from django import forms
+from django.forms import modelformset_factory
 
+
+from .models import (
+    Properties,
+    PropertyImage,
+    PurchaseOffer,
+)
 User = get_user_model()       
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['bio', 'profile_picture']
+        fields = ['role','bio', 'profile_picture']
 
 
 class PropertyForm(forms.ModelForm):
@@ -24,7 +32,7 @@ class PropertyForm(forms.ModelForm):
         model=Properties
         fields=['property_country', 'property_street', 'property_city', 'property_postal', 
                   'property_price', 'property_description', 'property_rooms', 'property_bedrooms', 
-                  'property_bathrooms', 'property_square_meters_size', 'property_sold_status', 'property_image']
+                  'property_bathrooms', 'property_square_meters_size', 'property_sold_status', 'property_image','property_type']
 
 
 class SignUpForm(UserCreationForm):
@@ -38,6 +46,22 @@ class ProfileForm(forms.ModelForm):
         model  = UserProfile
         fields = ("bio", "profile_picture")
 
+class PurchaseOfferForm(forms.ModelForm):
+    class Meta:
+        model = PurchaseOffer
+        fields = ('offer_price', 'expiration_date')
+        widgets = {
+            'expiration_date': forms.SelectDateWidget()
+        }
 
+PropertyImageFormSet = modelformset_factory(
+    PropertyImage,
+    fields=('image',),     # your ImageField on PropertyImage
+    extra=3,                # how many empty “upload” slots to show
+    max_num=10,             # limit total images
+    can_delete=True,        # allow deleting existing ones
+)
 # If you still reference "ProfileEditForm" in views:
 ProfileEditForm = ProfileForm     
+
+
